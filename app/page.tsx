08d99238921052c152ca6702ad084f6ede5b54ce
pabin-limbu <changsu.pabin@gmail.com>
2025-11-25 14:11:26 +0800

    cache added

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import {cacheLife} from "next/cache";
 
 const BASE_URL: string = process.env.NEXT_PUBLIC_BASE_URL ?? "";
 const Page = async () => {
+    'use cache'
+    cacheLife('hours')
 
     const response = await fetch(`${BASE_URL}/api/events`);
 
