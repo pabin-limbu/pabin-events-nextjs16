82a2932b753950a9f542d1b77cc5defe9c0c7f60
pabin-limbu <changsu.pabin@gmail.com>
2025-11-25 14:06:10 +0800

    cache removed

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,6 @@ import {cacheLife} from "next/cache";
 const BASE_URL: string = process.env.NEXT_PUBLIC_BASE_URL ?? "";
 const Page = async () => {
 
-    'use cache';
-    cacheLife('hours');
-    console.log("pabin")
-
     const response = await fetch(`${BASE_URL}/api/events`);
 
     const {events} = await response.json();
