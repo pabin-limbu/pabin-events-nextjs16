b07898b546c0603f820379247c41af73e55b9f18
pabin-limbu <changsu.pabin@gmail.com>
2025-11-27 17:28:22 +0800

    env updated

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ const BASE_URL: string = process.env.NEXT_PUBLIC_BASE_URL ?? "";
 const Page = async () => {
     'use cache'
     cacheLife('hours')
-    console.log("a")
+
 
     const response = await fetch(`${BASE_URL}/api/events`);
 
