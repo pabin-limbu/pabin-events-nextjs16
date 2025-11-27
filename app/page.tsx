b538208e801f37608ff94c3d5ad22dc8533cee64
pabin-limbu <changsu.pabin@gmail.com>
2025-11-27 17:08:36 +0800

    updated base url

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,17 @@ import ExploreBtn from "@/components/ExploreBtn";
 import EventCard from "@/components/EventCard";
 import {EventDocument} from "@/database";
 import {cacheLife} from "next/cache";
-import events from "@/lib/constants";
 
-// const BASE_URL: string = process.env.NEXT_PUBLIC_BASE_URL ?? "";
+
+const BASE_URL: string = process.env.NEXT_PUBLIC_BASE_URL ?? "";
 const Page = async () => {
     'use cache'
     cacheLife('hours')
     console.log("a")
 
-    // const response = await fetch(`${BASE_URL}/api/events`);
-    //
-    // const {events} = await response.json();
+    const response = await fetch(`${BASE_URL}/api/events`);
+
+    const {events} = await response.json();
 
 
     return (
